4bed907e4ecf46fa401291144f1f4de4265618ad
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-06-12 16:24:39 +0200

    Adjust to opendatabs

--- _templates/template_header.md
+++ _templates/template_header.md
@@ -1,24 +1,26 @@
-# Starter code for all datasets of the City of Munich
+# Starter code for all datasets of the Canton of Basel-Stadt
 
 
 💡 Have you ever **wished to be able to open any dataset in a data portal with one click and get a fully configured Python or R Markdown file? Like – with all metadata, descriptions, data links, and some starter code?** 
 
 This is an experiment to provide exactly this. 
 
-With this repo you get starter code for **all CSV data sets** (currently {{ DATASET_COUNT }}) that are available in the [open data portal of the city of Munich](https://opendata.muenchen.de/). 
+With this repo you get starter code for **all datasets** (currently {{ DATASET_COUNT }}) that are available in the [open data portal of the canton of Basel-Stadt](https://data.bs.ch). 
 
 In the list below you find links to preconfigured code files so that you **immediately can start analyzing the data with R or Python.**
 
 <!-- **The Jupyter Notebooks can be opened directly in Google Colab.** 🚀 -->
 
-Feedback is always very welcome – simply open an issue or write to 📧 [data.opengov@muenchen.de](mailto://data.opengov@muenchen.de)
+Feedback is always very welcome – simply open an issue or write to 📧 [opendata@bs.ch](mailto://opendata@bs.ch)
 
 ⚠️ **Note**: To download individual files from GitHub you need to switch to the file's `RAW` view and then `Right-Click-Save-As`. Only so you get the proper file format rather than the HTML that GitHub yields.
 
-For all data licenses please refer to each data set's link or the metadata API endpoint provided [here](https://opendata.muenchen.de/pages/nutzungsbedingungen).
+For all data licenses please refer to each data set's link or the metadata API endpoint provided [here](https://www.opendata.bs.ch/nutzungsbedingungen.html).
 
 🌻 Our thanks for the template and help goes to Patrick Arnecke from the Statistical Office of the Canton of Zurich. Here is the original:
 [https://openzh.github.io/starter-code-openZH/](https://openzh.github.io/starter-code-openZH/).
+
+🌻 Also many thanks to the Open Government Team of the City of Munich for providing the code to the generator: [https://github.com/it-at-m/startercode-generator-munich](https://github.com/it-at-m/startercode-generator-munich)
 
 <sub>Latest update: **{{ TODAY_DATE }}**<sub>
 
--- updater.py
+++ updater.py
@@ -1,13 +1,12 @@
 # IMPORTS -------------------------------------------------------------------- #
 
 import pandas as pd
-import numpy as np
 import requests
 import json
 import re
-import copy
 from datetime import datetime
 from tqdm import tqdm
+import credentials
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -16,14 +15,14 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # CONSTANTS ------------------------------------------------------------------ #
 
 PATH_METADATA = "_metadata_json/"
-BASELINK_DATASHOP = "https://www.zh.ch/de/politik-staat/opendata.html#/datasets/"
+BASELINK_DATASHOP = "https://data.bs.ch/explore/dataset/"
 
-PROVIDER = "Canton Zurich"
-SHOP_METADATA_LINK = "https://www.web.statistik.zh.ch/ogd/daten/zhweb.json"
-SHOP_ABBR = "ktzh"
+PROVIDER = "Statistisches Amt des Kantons Basel-Stadt - Fachstelle OGD"
+SHOP_METADATA_LINK = "https://data.bs.ch/api/explore/v2.1/catalog/datasets/100057/exports/json"
+CONTACT = "Fachstelle für OGD Basel-Stadt | opendata@bs.ch"
 
-GITHUB_ACCOUNT = "TxominBasterraChang"
-REPO_NAME = "Results"
+GITHUB_ACCOUNT = "opendatabs"
+REPO_NAME = "startercode"
 REPO_BRANCH = "main"
 REPO_R_MARKDOWN_OUTPUT = "01_r-markdown/"
 REPO_PYTHON_OUTPUT = "02_python/"
@@ -41,47 +40,26 @@ TODAY_DATETIME = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 TITLE_MAX_CHARS = 200
 
 # select metadata features that are going to be displayed in starter code files
-KEYS_DATASET = ['issued', 'modified', 'startDate', 'endDate',
-                'theme', 'keyword', 'publisher', 'landingPage']
-
-KEYS_DISTRIBUTION = ['ktzhDistId', 'title', "description",
-                     'issued', 'modified', "rights"]
+KEYS_DATASET = ['dataset_identifier', 'title', 'description', 'contact_name',
+                'issued', 'modified', 'rights',
+                'temporal_coverage_start_date', 'temporal_coverage_end_date',
+                'themes', 'keywords', 'publisher', 'reference']
 
 
 # FUNCTIONS ------------------------------------------------------------------ #
-
 def get_current_json():
     """Request metadata catalogue from data shop"""
-    res = requests.get(SHOP_METADATA_LINK)
+    res = requests.get(SHOP_METADATA_LINK, proxies=credentials.proxies)
     # # save with date to allow for later error and change analysis
     # with open(f"{PATH_METADATA}{TODAY_DATE}.json", "wb") as file:
-    #     file.write(res.content)
+    # file.write(res.content)
     data = json.loads(res.text)
-    data = pd.DataFrame(data['dataset'])
-    return data
-
-
-def has_csv_distribution(dists):
-    """Iterate over distributions and keep only CSV entries"""
-    csv_dists = [x for x in dists if "CSV" in x.get("format", "")]
-    if csv_dists != []:
-        return csv_dists
-    else:
-        return np.nan
-
-
-def filter_csv(all_data):
-    """Filter out CSV distributions"""
-    data = copy.deepcopy(all_data)
-    data.distribution = data.distribution.apply(has_csv_distribution)
-    data.dropna(subset=["distribution"], inplace=True)
-    data.reset_index(drop=True, inplace=True)
-    return data
+    return pd.DataFrame(data)
 
 
 def sort_data(data):
     """Sort by integer prefix of identifier"""
-    data["id_short"] = df.identifier.apply(
+    data["id_short"] = df.dataset_identifier.apply(
         lambda x: x.split("@")[0]).astype(int)
     data.sort_values("id_short", inplace=True)
     data.reset_index(drop=True, inplace=True)
@@ -91,33 +69,11 @@ def sort_data(data):
 def prepare_data_for_codebooks(data):
     """Prepare metadata from catalogue in order to create code files"""
     data["metadata"] = None
-    data["contact"] = None
-    data["distributions"] = None
-    data["distribution_links"] = None
 
     # iterate over all datasets and compose refined data for markdown and code cells
     for idx in tqdm(data.index):
-        md = [
-            f"- **{k.capitalize()}** `{data.loc[idx, k]}`\n" for k in KEYS_DATASET]
+        md = [f"- **{k.capitalize()}** `{data.loc[idx, k]}`\n" for k in KEYS_DATASET]
         data.loc[idx, "metadata"] = "".join(md)
-        contact_data = df.loc[idx, "contactPoint"][0].values()
-        contact_data = [x for x in contact_data if x != None]
-        data.loc[idx, "contact"] = " | ".join(contact_data)
-
-        tmp_dists = []
-        tmp_links = []
-        for dist in data.loc[idx, "distribution"]:
-            # remove line breaks of description since these break the comment blocks
-            if dist["description"] != None:
-                dist["description"] = re.sub(r"\n+", " ", dist["description"])
-            md = [f"# {k.capitalize():<25}: {dist[k]}\n" for k in KEYS_DISTRIBUTION]
-            tmp_dists.append("".join(md))
-            tmp_links.append(dist["downloadUrl"])
-
-        # use .at[] to properly add a list as value to row
-        # https://stackoverflow.com/a/53299945/7117003
-        data.at[idx, "distributions"] = tmp_dists
-        data.at[idx, "distribution_links"] = tmp_links
 
     return data
 
@@ -129,7 +85,7 @@ def create_python_notebooks(data):
             py_nb = file.read()
 
         # populate template with metadata
-        identifier = data.loc[idx, "identifier"]
+        identifier = data.loc[idx, "dataset_identifier"]
         py_nb = py_nb.replace("{{ PROVIDER }}", PROVIDER)
         py_nb = py_nb.replace("{{ DATASET_TITLE }}", re.sub(
             "\"", "\'", data.loc[idx, "title"]))
@@ -139,33 +95,19 @@ def create_python_notebooks(data):
         py_nb = py_nb.replace("{{ DATASET_IDENTIFIER }}", identifier)
         py_nb = py_nb.replace("{{ DATASET_METADATA }}", re.sub(
             "\"", "\'", data.loc[idx, "metadata"]))
-        py_nb = py_nb.replace("{{ DISTRIBUTION_COUNT }}", str(
-            len(data.loc[idx, "distributions"])))
 
         ds_link = f'[Direct data shop link for dataset]({BASELINK_DATASHOP}{identifier})'
         py_nb = py_nb.replace("{{ DATASHOP_LINK }}", ds_link)
-        py_nb = py_nb.replace("{{ CONTACT }}", data.loc[idx, "contact"])
+
+        download_link = f"{BASELINK_DATASHOP}{identifier}/download"
+        code_block = f"df = get_dataset('{download_link}')"
+        py_nb = py_nb.replace("{{ LOAD_DATA }}", code_block)
+
+        py_nb = py_nb.replace("{{ CONTACT }}", CONTACT)
 
         # to properly populate the code cell for data set import
         # we need to operate on the actual JSON rather than use simple string replacement
         py_nb = json.loads(py_nb, strict=False)
-
-        # find predefined code cell for distributions
-        # this cell contains just the string '{{ DISTRIBUTION }}'
-        # this has to be set in the template
-        for dist_idx, cell in enumerate(py_nb["cells"]):
-            if cell["source"] == ['{{ DISTRIBUTION }}']:
-                dist_cell_idx = dist_idx
-                break
-
-        # create metadata and code blocks for each CSV distribution
-        code_block = []
-        for id_dist, (dist, dist_link) in enumerate(zip(data.loc[idx, "distributions"], data.loc[idx, "distribution_links"])):
-            code = f"# Distribution {id_dist}\n{dist}\ndf = get_dataset('{dist_link}')\n"
-            code = "".join([f'{line}\n' for line in code.split("\n")])
-            code_block.append(code)
-        code_block = "".join(code_block)
-        py_nb["cells"][dist_cell_idx]["source"] = code_block
 
         # save to disk
         with open(f'{TEMP_PREFIX}{REPO_PYTHON_OUTPUT}{identifier}.ipynb', 'w') as file:
@@ -179,7 +121,7 @@ def create_rmarkdown(data):
             rmd = file.read()
 
         # populate template with metadata
-        identifier = data.loc[idx, "identifier"]
+        identifier = data.loc[idx, "dataset_identifier"]
         rmd = rmd.replace("{{ DATASET_TITLE }}", data.loc[idx, "title"])
         rmd = rmd.replace("{{ PROVIDER }}", PROVIDER)
         rmd = rmd.replace("{{ TODAY_DATE }}", TODAY_DATE)
@@ -188,29 +130,23 @@ def create_rmarkdown(data):
                           data.loc[idx, "description"])
         rmd = rmd.replace("{{ DATASET_METADATA }}", data.loc[idx, "metadata"])
 
-        rmd = rmd.replace("{{ CONTACT }}", data.loc[idx, "contact"])
-        rmd = rmd.replace("{{ DISTRIBUTION_COUNT }}", str(
-            len(data.loc[idx, "distributions"])))
-
         ds_link = f'[Direct data shop link for dataset]({BASELINK_DATASHOP}{identifier})'
         rmd = rmd.replace("{{ DATASHOP_LINK }}", ds_link)
 
-        # create code blocks for all distributions
-        code_block = []
-        for id_dist, (dist, dist_link) in enumerate(zip(data.loc[idx, "distributions"], data.loc[idx, "distribution_links"])):
-            code = f"# Distribution {id_dist}\n{dist}\ndf <- read_delim('{dist_link}')\n\n"
-            code_block.append(code)
-        code_block = "".join(code_block)
-        rmd = rmd.replace("{{ DISTRIBUTIONS }}", "".join(code_block))
+        download_link = f"{BASELINK_DATASHOP}{identifier}/download?format=csv&timezone=Europe%2FZurich"
+        code_block = f"df <- get_dataset('{download_link}')"
+        rmd = rmd.replace("{{ LOAD_DATA }}", code_block)
+
+        rmd = rmd.replace("{{ CONTACT }}", CONTACT)
 
         # save to disk
-        with open(f'{TEMP_PREFIX}{REPO_R_MARKDOWN_OUTPUT}{identifier}.Rmd', 'w') as file:
+        with open(f'{TEMP_PREFIX}{REPO_R_MARKDOWN_OUTPUT}{identifier}.Rmd', 'w', encoding='utf-8') as file:
             file.write("".join(rmd))
 
 
 def get_header(dataset_count):
     """Retrieve header template and populate with date and count of data records"""
-    with open(f"{TEMPLATE_FOLDER}{TEMPLATE_HEADER}") as file:
+    with open(f"{TEMPLATE_FOLDER}{TEMPLATE_HEADER}", encoding='utf-8') as file:
         header = file.read()
     header = re.sub("{{ DATASET_COUNT }}", str(int(dataset_count)), header)
     header = re.sub("{{ TODAY_DATE }}", TODAY_DATETIME, header)
@@ -230,7 +166,7 @@ def create_overview(data, header):
     md_doc.append("| :-- | :-- | :-- | :-- | :-- |\n")
 
     for idx in tqdm(data.index):
-        identifier = data.loc[idx, "identifier"]
+        identifier = data.loc[idx, "dataset_identifier"]
         # remove square brackets from title, since these break markdown links
         title_clean = data.loc[idx, "title"].replace(
             "[", " ").replace("]", " ")
@@ -248,15 +184,14 @@ def create_overview(data, header):
 
     md_doc = "".join(md_doc)
 
-    with open(f"{TEMP_PREFIX}README.md", "w") as file:
+    with open(f"{TEMP_PREFIX}README.md", "w", encoding='utf-8') as file:
         file.write(md_doc)
 
 
 # CREATE CODE FILES ---------------------------------------------------------- #
 
-all_data = get_current_json()
+df = get_current_json()
 
-df = filter_csv(all_data)
 df = sort_data(df)
 df = prepare_data_for_codebooks(df)
 
