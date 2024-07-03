dbcc717e40f3d47d0c57889b868a46489ad09cf3
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-07-03 19:02:23 +0200

    Add second Dockerfile

---
+++ update_github.sh
@@ -0,0 +1,6 @@
+git config --global --add safe.directory /code/startercode-generator-bs/_work
+cd /code/startercode-generator-bs/_work
+git pull
+git add .
+git commit -m "Regular update of all starter codes"
+git push
