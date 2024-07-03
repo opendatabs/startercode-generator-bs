fbf736749ba20be7d71064894d58eab28704c06e
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-07-03 19:34:53 +0200

    Update script

--- update_github.sh
+++ update_github.sh
@@ -1,5 +1,7 @@
+GITHUB_TOKEN=$(cat ~/.github_token)
 git config --global --add safe.directory /code/startercode-generator-bs/_work
 cd /code/startercode-generator-bs/_work
+git remote set-url origin https://osaeedi:${GITHUB_TOKEN}@github.com/opendatabs/startercode-opendatabs.git
 git pull
 git add .
 git commit -m "Regular update of all starter codes"
