661663ad23cf88d96e519613d215908642fa90b7
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-10-23 15:54:34 +0200

    Add script for renku

--- update_renku.sh
+++ update_renku.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Load the GitHub token from a file
-GITHUB_TOKEN=$(cat /root/.github_token)
+# Load the GitLab token from a file
+GITLAB_TOKEN=$(cat /root/.renku_token)
 # Load Git user name and email from the configuration file
 source /root/.gitconfig_info
 # Configure git to use the token for authentication
-git config --global --add safe.directory /code/startercode-generator-bs/_work
+git config --global --add safe.directory /code/startercode-generator-bs/_work_renku
 # Configure user name and email for Git commits
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
 
 # Change to the appropriate directory
-cd /code/startercode-generator-bs/_work
-# Set the remote URL with the token
-git remote set-url origin https://$GIT_USER_NAME:${GITHUB_TOKEN}@github.com/opendatabs/startercode-opendatabs.git
+cd /code/startercode-generator-bs/_work_renku
+# Set the remote URL with the token (GitLab URL)
+git remote set-url origin https://$GIT_USER_NAME:${GITLAB_TOKEN}@gitlab.renkulab.io/opendatabs/startercode-opendatabs.git
 # Pull the latest changes
 git pull
 
